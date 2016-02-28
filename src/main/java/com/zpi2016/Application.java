bb96b61195d89cb1a53c65085cd8d0ae4432769e
Jakub <kuba.amanowicz@gmail.com>
2016-02-28 17:49:12 +0100

    working model with h2 database

--- src/main/java/com/zpi2016/Application.java
+++ src/main/java/com/zpi2016/Application.java
@@ -1,9 +1,12 @@
 package com.zpi2016;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -35,5 +38,15 @@ public class Application{
 		SpringApplication.run(Application.class, args);
 	}
 
+	/**
+	 * Enable H2 database console
+	 * Usage: /console
+	 */
+	@Bean
+	ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
 
 }
