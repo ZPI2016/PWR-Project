af040007d949bd342178534503b2d9d81949468a
Martyna Litkowska <martyna.litkow@gmail.com>
2016-04-15 13:03:27 +0200

    adding wro...

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -3,7 +3,7 @@
  */
 
 (function(){
-    var app = angular.module('myApp', []);
+    var app = angular.module('myApp', ['ngRoute']);
     var login = false;
 
     app.controller('FormController', function ($http){
