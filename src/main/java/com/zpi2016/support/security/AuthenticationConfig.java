ac65d9df8ecfb79e4c7aafc7b11502e13f6ec91e
Jakub Amanowicz <kuba.amanowicz@gmail.com>
2016-04-23 10:35:54 +0200

    psotgres not working yet

--- src/main/java/com/zpi2016/support/security/AuthenticationConfig.java
+++
@@ -1,7 +0,0 @@
-package com.zpi2016.support.security;
-
-/**
- * Created by aman on 23.04.16.
- */
-public class AuthenticationConfig {
-}
--- src/main/java/com/zpi2016/support/security/SecurityConfig.java
+++ src/main/java/com/zpi2016/support/security/SecurityConfig.java
@@ -1,19 +1,36 @@
 package com.zpi2016.support.security;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * The SecurityConfiguration class enables route access configuration
  */
+@Configuration
 @EnableWebSecurity
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public SecurityConfig(){}
+    @Autowired
+    private DataSource dataSource;
+
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password, enabled from users where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username, role where username=?");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,4 +45,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // TYMCZASOWO:
         http.csrf().disable();
     }
+
+    @Configuration
+    public class DataSourceConfig extends WebMvcConfigurerAdapter {
+
+        @Bean(name = "dataSource")
+        public DriverManagerDataSource dataSource() {
+            DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+            driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+            driverManagerDataSource.setUrl("jdbc:postgresql://localhost:5432/organizer");
+            driverManagerDataSource.setUsername("organizerusr");
+            driverManagerDataSource.setPassword("orgpass");
+            return driverManagerDataSource;
+        }
+
+    }
+
 }
