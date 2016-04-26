f69163005caa9d566f4663340751c424d15d7a55
Filip Chrześcijanek <filip702@gmail.com>
2016-04-27 00:10:53 +0200

    pushing

--- src/main/java/com/zpi2016/Application.java
+++ src/main/java/com/zpi2016/Application.java
@@ -32,7 +32,7 @@ public class Application{
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
-
+ 
 	@PostConstruct
 	public void init() {
 		userService.save(
