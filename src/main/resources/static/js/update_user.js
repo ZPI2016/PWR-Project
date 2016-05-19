3b001c7ae1ee4fc247d10881953dcc6375b0730d
Cledyn <ssandra.kk@gmail.com>
2016-05-19 14:07:44 +0200

    Removed redundant libs (angular, jquery), refactored update user template

--- src/main/resources/static/js/update_user.js
+++ src/main/resources/static/js/update_user.js
@@ -6,21 +6,13 @@
 
 
     app.controller('UserUpdateController', function ($scope, $http) {
-
-        $http.get('/users/username/martyna').success(function (result) {
-            $scope.userData = result;
+        $http.get('/users/security/logged').success(function (result) {
             initializeFormFields(result);
-        })
-            .error(function (data, status, header, config) {
-                    console.log("Error during getting user data.");
-                    $scope.ServerResponse = htmlDecode("Data: " + data +
-                    "\n\n\n\nstatus: " + status +
-                    "\n\n\n\nheaders: " + header +
-                    "\n\n\n\nconfig: " + config);
-            });
+        });
         var myapp = this;
         this.onClick = function (user) {
             $http.get('/users/security/logged').success(function (result) {
+                // initializeFormFields(result);
                 oldUserData = {
                     userID: result.id,
                     email: result.email,
@@ -40,8 +32,8 @@
                 email: user.email,
                 address: user.address,
                 dob: user.dob,
-               // firstName: user.firstName,
-               //lastName: user.lastName,
+                // firstName: user.firstName,
+                //lastName: user.lastName,
                 radius: user.radius
             });
             $http.put('/users/security/logged', myapp.data).success(function (data, status, headers) {
@@ -50,6 +42,7 @@
 
             })
                 .error(function (data, status, header, config) {
+
                     console.log("Error during updating user data.");
                     $scope.ServerResponse = htmlDecode("Data: " + data +
                     "\n\n\n\nstatus: " + status +
@@ -60,7 +53,8 @@
 
 
     })
-    function initializeFormFields(result){
+    function initializeFormFields(result) {
+        console.log(result.username);
         document.getElementById("username").value = result.username;
         document.getElementById("email").value = result.email;
         document.getElementById("geoLatitude").value = result.geoLatitude;
